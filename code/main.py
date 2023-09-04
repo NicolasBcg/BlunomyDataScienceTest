2b120873f7f0d4006938282eca743f6c03d0b27d
NicolasBcg <82137844+NicolasBcg@users.noreply.github.com>
2023-09-04 23:36:02 +0200

    display planes
    seems that the planes equations aren't the ones we are looking for

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ from sklearn.cluster import AgglomerativeClustering
 
 colors=['#808080', '#FF0000', '#FFFF00', '#00FF00', '#0000FF', '#FF00FF', '#C0C0C0', '#FFA500', '#191970', '#17becf', '#FF69B4', '#8B008B', '#6B8E23', '#00BFFF'] #a bunch of color to display different labels
 
-def plot(array=[],number_of_labels=0):#a function to visualise 3D area. no number_of_label if there are no labels
+def plot(array=[],number_of_labels=0,planes=[]):#a function to visualise 3D area. no number_of_label if there are no labels
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     if number_of_labels==0:
@@ -18,8 +18,16 @@ def plot(array=[],number_of_labels=0):#a function to visualise 3D area. no numbe
         for label_number in range(number_of_labels):
             for point in array[label_number]:
                 ax.scatter(point[0], point[1], point[2],c=colors[label_number])
-
-
+    for plane in planes:
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
+                        np.arange(ylim[0], ylim[1]))
+        Z = np.zeros(X.shape)
+        for r in range(X.shape[0]):
+            for c in range(X.shape[1]):
+                Z[r,c] = plane[0] * X[r,c] + plane[1] * Y[r,c] + plane[2]
+        ax.plot_wireframe(X,Y,Z, color='k')
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
@@ -35,7 +43,7 @@ def cluster(data_numpy_array):
         data_by_label[clustering.labels_[i]].append(data_numpy_array[i])
     return data_by_label
 
-def findPlane(array):
+def findPlane(array): #find the nearest plane to an array of points
     tmp_A = []
     tmp_b = []
     for point in array:
@@ -44,18 +52,26 @@ def findPlane(array):
     b = np.matrix(tmp_b).T
     A = np.matrix(tmp_A)
     fit = (A.T * A).I * A.T * b
+    errors = b - A * fit
+    residual = np.linalg.norm(errors)
     print("solution:")
     print("%f x + %f y + %f = z" % (fit[0], fit[1], fit[2]))
+    print("errors:")
+    print(errors)
+    print("residual:")
+    print(residual)
     return fit[0], fit[1], fit[2]
+
 
 def main():  
     df = pd.read_parquet('../data/lidar_cable_points_easy.parquet') #extracting data into df
     data_numpy_array = df.to_numpy()#create a numpy array from data frame
-    #plot(data_numpy_array)#plot array
+    #plot(data_numpy_array)
     data_by_label = cluster(data_numpy_array)
-    print("there are "+str(len(data_by_label))+" wires detected by clustering") 
-    findPlane(data_by_label[0])
     #plot(data_by_label,len(data_by_label))
+    print("there are "+str(len(data_by_label))+" wires detected by clustering") 
+    planes=[findPlane(array) for array in data_by_label]
+    plot(data_by_label,len(data_by_label),planes)
 
 if __name__ == '__main__' :
     main()
