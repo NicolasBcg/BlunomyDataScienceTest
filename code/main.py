56a9e2df3b51e60bad484cbb6dde93065dd58bd8
NicolasBcg <82137844+NicolasBcg@users.noreply.github.com>
2023-09-04 20:06:24 +0200

    Detecting wires with AgglomerativeClustering
    Added Agglomerative Clustering to assign wires to the points. Seems good for the easy batch of points but not for the others.
    Next step is to create a function to visualise the result of clustering.

--- code/main.py
+++ code/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
+
 def plot(df):#a function to visualise data frame
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -18,6 +21,10 @@ def plot(df):#a function to visualise data frame
 def main():  
     df = pd.read_parquet('../data/lidar_cable_points_easy.parquet') #extracting data into df
     #plot(df)
-    print(df)
+    #print(df)
+    data_numpy_array = df.to_numpy()#create a numpy array from data frame
+    clustering = AgglomerativeClustering(n_clusters=None,linkage='single',distance_threshold=0.75).fit(data_numpy_array)#link points/agglomerations when the minimum distance between the points/aglomerations is inferior to distance_threshold with all the points in data_numpy_array
+    #print("there are "+str(clustering.n_clusters_)+" wires detected by clustering") 
+
 if __name__ == '__main__' :
     main()
